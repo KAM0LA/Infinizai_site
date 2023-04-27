220d17d7bc5a96661a273e7842ad5f15676ccc9e
Kamola0911 <kamola092002@mail.ru>
2023-04-27 11:42:34 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let modeBtn = document.getElementById("dark-light");
+
+modeBtn.addEventListener("click", function () {
+  if (document.body.className != "dark") {
+    this.firstElementChild.src = "images/light.svg";
+  } else {
+    this.firstElementChild.src = "images/dark.svg";
+  }
+  document.body.classList.toggle("dark");
+});
+
+// Loader
+
+const loading = document.querySelector(".wait");
+setTimeout(() => {
+    loading.classList.add('loading-none');
+}, 2000);
